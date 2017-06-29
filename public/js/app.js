23cd059ebb1349a8ff6481192c0498b5ed9e9707
Devon Johnson <dljohnson@rollins.edu>
2017-06-29 16:31:52 -0400

    iphone 6 and iphon6+ responsive separate @media queries added

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,1 @@
-$(window).resize(function () {
-    var viewportWidth = $(window).width();
-    if (viewportWidth < 480) {
-            $(".navbar-fixed-top").removeClass("navbar-fixed-top").addClass("navbar-static-top");
-    }
-})
+
